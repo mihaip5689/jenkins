d3a8874e9691b4549f55fed6215afcc577f5e17b
mihaip5689 <73463717+mihaip5689@users.noreply.github.com>
2023-02-09 22:53:28 +0200

    Update checkSecurityVulnerabilities.py

--- checkSecurityVulnerabilities.py
+++ checkSecurityVulnerabilities.py
@@ -8,6 +8,7 @@ print(configuration)
 spec = configuration['spec']
 containers = spec['containers']
 
+print('Checking user ID and group ID for pod: ' + configuration['metadata']['name'])
 if 'securityContext' not in spec:
     print('Insecure pod:: Security Context not configured. Applying default configuration.')
     configuration['spec']['securityContext'] = {'runAsUser': 1000, 'runAsGroup': 1000}
@@ -23,13 +24,59 @@ else:
 
 for ct in configuration['spec']['containers']:
     if 'securityContext' not in ct:
-        print('Insecure container:: Security Context not configured. Applying default configuration.')
-        ct['securityContext'] = {'allowPrivilegeEscalation': False}
+        print('Insecure container:: Security Context not configured.')
+        print('Applying default configuration for container: ' + ct['name'])
+        print('Disabling privilege escalation')
+        ct['securityContext']['allowPrivilegeEscalation'] = False
+        print('Setting default user ID and group ID')
+        ct['securityContext']['runAsUser'] = 1000
+        ct['securityContext']['runAsGroup'] = 1000
+        print('Setting read only root file system')
+        ct['securityContext']['readOnlyRootFileSystem'] = True
     else:
         sC = ct['securityContext']
+        if 'runAsUser' in sC and sC['runAsUser'] == 0:
+            print('Insecure container. Trying to run as root.')
+            print('Setting default user ID for container: ' + ct['name'])
+            ct['securityContext']['runAsUser'] = 1000
+        if 'runAsGroup' in sC and sC['runAsGroup'] == 0:
+            print('Insecure container. Trying to run as root.')
+            print('Setting default group ID for container: ' + ct['name'])
+            ct['securityContext']['runAsGroup'] = 1000
         if sC['allowPrivilegeEscalation'] == 'true':
-            print('Insecure container:: Allowed privilege escalation. Applying default configuration.')
-            ct['securityContext'] = {'allowPrivilegeEscalation': False}
+            print('Insecure container:: Allowed privilege escalation.')
+            print('Disabling privilege escalation for container: ' + ct['name'])
+            ct['securityContext']['allowPrivilegeEscalation'] = False
+        if sC['readOnlyRootFileSystem'] == 'false':
+            print('Insecure container:: Allowed access to root file system.')
+            print('Setting read only root file system for container: ' + ct['name'])
+            ct['securityContext']['readOnlyRootFileSystem'] = True
+
+
+# for ct in configuration['spec']['containers']:
+#     print('Checking user ID and group ID for container: ' + ct['name'])
+#     if 'securityContext' not in ct:
+#         print('Insecure container:: Security Context not configured. Applying default configuration.')
+#         print('Disabling privilege escalation')
+#         ct['securityContext'] = {'allowPrivilegeEscalation': False}
+#     else:
+#         sC = ct['securityContext']
+#         if sC['allowPrivilegeEscalation'] == 'true':
+#             print('Insecure container:: Allowed privilege escalation. Applying default configuration.')
+#             ct['securityContext'] = {'allowPrivilegeEscalation': False}
+#
+#
+# for ct in configuration['spec']['containers']:
+#     print('Checking privilege escalation for container: ' + ct['name'])
+#     if 'securityContext' not in ct:
+#         print('Insecure container:: Security Context not configured. Applying default configuration.')
+#         ct['securityContext'] = {'allowPrivilegeEscalation': False}
+#     else:
+#         sC = ct['securityContext']
+#         if sC['allowPrivilegeEscalation'] == 'true':
+#             print('Insecure container:: Allowed privilege escalation. Applying default configuration.')
+#             ct['securityContext'] = {'allowPrivilegeEscalation': False}
+
 configuration['metadata']['name'] = 'secure-pod'
 
 with open('securePodToDeploy.yaml', 'w') as f:
